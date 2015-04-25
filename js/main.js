e19c81e863521de739d326571f27bc97c519c40c
drewcovi <drew.covi@me.com>
2015-04-25 15:31:17 -0500

    always moving!

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ $('.flip')
     location.href="https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&es_th=1&ie=UTF-8#q=zerg%20rush&es_th=1"
   })
   .hover(function(){
-    if(Math.round(Math.random())){
+    // if(Math.round(Math.random())){
       $(this).css("transform", 'translate('+Math.random()*200+'px,'+Math.random()*200+'px)')
-    }else{
+    // }else{
 
-    }
+    // }
   })
