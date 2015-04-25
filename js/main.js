39a6a4090e803d6558c26eb9a091857c1d3437b2
drewcovi <drew.covi@me.com>
2015-04-25 15:20:05 -0500

    TROLLOLOLOL

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$('.flip').click(function(){
-  location.href="https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&es_th=1&ie=UTF-8#q=zerg%20rush&es_th=1"
-})
+$('.flip')
+  .click(function(){
+    location.href="https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&es_th=1&ie=UTF-8#q=zerg%20rush&es_th=1"
+  })
+  .hover(fucntion(){
+    if(Math.round(Math.random())){
+      $(this).css("transform", 'translate('+Math.random()*200+'px,'+Math.random()*200+'px)')
+    }else{
+      
+    }
+  })
