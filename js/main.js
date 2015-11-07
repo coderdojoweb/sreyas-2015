ae5bca1fc056f6f3430b04c84285d976b719f27e
drewcovi <drew.covi@me.com>
2015-11-07 15:16:52 -0600

    new site

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-$('.flip')
-  .click(function(){
-    location.href="https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&es_th=1&ie=UTF-8#q=zerg%20rush&es_th=1"
-  })
-  .hover(function(){
-    // if(Math.round(Math.random())){
-      $(this).css("transform", 'translate('+Math.random()*200+'px,'+Math.random()*200+'px)')
-    // }else{
+$("img").click(function(){
+alert("ouch!");
+$("body").css("background","black")
 
-    // }
-  })
+});
